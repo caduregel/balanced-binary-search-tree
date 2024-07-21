8a57f3f118789e609feb67a2109b2b8973470f8b
liam <liam@nehoray.net>
2024-07-21 14:16:19 +0200

    Updated changed array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { prettyPrint, Tree } from "./tree.js";
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+const tree = new Tree([42, 17, 88, 56, 33, 71, 29, 5, 92, 48, 60, 77, 12, 81, 23, 34, 66, 90, 53, 6])
 
 prettyPrint(tree.root)
